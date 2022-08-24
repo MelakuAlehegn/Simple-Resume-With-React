53fdd55e21eff9932016b4cb8c7822558d212840
melakualehegn <melakualehegn34@gmail.com>
2022-08-24 23:28:40 +0300

    Input/button Css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Experience from './components/Experience';
 import Header from './components/Header';
 import Skills from './components/Skills';
+import Input from './components/Input';
+import Button from './components/Buttons';
+import './index.css'
+
 
 export default function App() {
   return (
@@ -9,6 +13,8 @@ export default function App() {
       <Header />
       <Experience />
       <Skills />
+      <Input />
+      <Button />
     </div>
   );
 }
